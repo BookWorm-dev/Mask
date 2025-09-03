06ffc409ac38e115f014feaf4562dea544cf60ef
Kristina <kgritsenko.2017@gmail.com>
2025-09-03 16:12:13 +0300

    feat: mobile responsive (header, hero, and info section) + mobile menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,19 @@
 const body = document.querySelector('body');
+
+const menuButton = document.querySelector('.menu-button');
+const nav = document.querySelector('.header__nav');
+const navClose = document.querySelector('.nav-close');
+
+menuButton.addEventListener('click', () => {
+	nav.classList.add('header__nav--active');
+});
+
+navClose.addEventListener('click', () => {
+	nav.classList.remove('header__nav--active');
+});
+
+document.addEventListener('click', e => {
+	if (!nav.contains(e.target) && !menuButton.contains(e.target)) {
+		nav.classList.remove('header__nav--active');
+	}
+});
